f08882ca05cb420760df4c03b422459d1f445270
OharaSorata <oliveiraguga2020guga@gmail.com>
2024-11-28 22:31:29 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('revealButton').addEventListener('click', function() {
+    document.getElementById('card1').classList.add('revealed');
+    document.getElementById('card2').classList.add('revealed');
+});
